25723fbc5b6501af6d605ff126af82f72fd01e9a
Mike Fulton <mikefultonpersonal@gmail.com>
2022-11-07 18:22:52 -0800

    add in git and tar as tools we want

--- tools/src/zopen_boot_uri.h
+++ tools/src/zopen_boot_uri.h
@@ -3,5 +3,5 @@
   #define ZOPEN_BOOT_URI_PREFIX "ZOSOpenTools"
   #define ZOPEN_BOOT_URI_SUFFIX "releases/download/boot"
   #define ZOPEN_BOOT_URI_TYPE "zos.pax.Z"
-  #define ZOPEN_BOOT_PKG { "gzip", "curl", "make", "utils", NULL }
+  #define ZOPEN_BOOT_PKG { "gzip", "tar", "curl", "make", "utils", "git", NULL }
 #endif
