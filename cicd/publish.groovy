fc369101405243dce93e0343fbe2f2eae4dc947f
Igor Todorovski <itodorov@ca.ibm.com>
2023-02-03 21:29:34 -0500

    Fix bolding

--- cicd/publish.groovy
+++ cicd/publish.groovy
@@ -50,7 +50,7 @@ DESCRIPTION="${DESCRIPTION}<b>Runtime Dependencies:</b> ${DEPENDENCIES}<br />"
 
 URL_LINE="https://github.com/ZOSOpenTools/${GITHUB_REPO}/releases/download/${GITHUB_REPO}_${BUILD_ID}/$PAX_BASENAME"
 DESCRIPTION="${DESCRIPTION}<br /><b>Command to download and install on z/OS:</b> <pre>pax -rf <(curl -o - -L ${URL_LINE}) && cd $DIR_NAME && . ./.env</pre>"
-DESCRIPTION="${DESCRIPTION}<br /><b>Or use: <pre>zopen install ${PORT_NAME}</pre>"
+DESCRIPTION="${DESCRIPTION}<br /><b>Or use:</b> <pre>zopen install ${PORT_NAME}</pre>"
 
 exists=$(github-release info -u ${GITHUB_ORGANIZATION} -r ${GITHUB_REPO}  -j)
 if [ $? -gt 0 ]; then
