eab1b82e5b50d7465a0f93932dec9e9115b4a4d6
Igor Todorovski <itodorov@ca.ibm.com>
2023-02-17 15:00:01 -0500

    Script to automatically add teams to all repos

---
+++ tools/addteam.py
@@ -0,0 +1,25 @@
+import sys
+from github import Github
+
+# create a Github instance
+if os.getenv('GITHUB_OAUTH_TOKEN') is None:
+  print("error: environment variable GITHUB_OAUTH_TOKEN must be defined");
+  sys.exit(1);
+
+# Github Enterprise with custom hostname
+g = Github(os.getenv('GITHUB_OAUTH_TOKEN'))
+
+# get the organization by name
+org = g.get_organization("ZOSOpenTools")
+
+# get the team by name
+# get the team by name (passed as argument)
+team_name = sys.argv[1]
+team = org.get_team_by_slug(team_name)
+
+# get all the repositories in the organization
+repos = org.get_repos()
+
+# add the team to each repository
+for repo in repos:
+    team.add_to_repos(repo)
