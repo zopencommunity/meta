5dc9bc60a3c7834f2574c53b8fce8bf1b8c9ee80
Igor Todorovski <itodorov@ca.ibm.com>
2023-10-06 15:38:12 -0400

    Add script to checking format

---
+++ tools/format_and_check_scripts.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+format=false
+shellcheck=false
+verbose=false
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -f | --format)
+      format=true
+      ;;
+    -s | --shellcheck)
+      shellcheck=true
+      ;;
+    -v | --verbose)
+      verbose=true
+      ;;
+    *)
+      if [ -f "$1" ]; then
+        files="$files $1"
+      else
+        echo "Ignoring invalid file: $1"
+      fi
+      ;;
+  esac
+  shift
+done
+
+if [ "$format" = false ] && [ "$shellcheck" = false ]; then
+  echo "Usage: $0 [-f | --format] [-s | --shellcheck] [-v | --verbose] [file1.sh file2.sh ...]"
+  exit 1
+fi
+
+if [ "$format" = true ]; then
+  for file in $files; do
+    if [ "$verbose" = true ]; then
+      echo "Formatting $file..."
+    fi
+    shfmt -i 2 -fn -w "$file"
+  done
+fi
+
+if [ "$shellcheck" = true ]; then
+  for file in $files; do
+    if [ "$verbose" = true ]; then
+      echo "Running ShellCheck on $file..."
+    fi
+    shellcheck -s sh -e SC2034,SC2048 --severity=error "$file"
+  done
+fi
