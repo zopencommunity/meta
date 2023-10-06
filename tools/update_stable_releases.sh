841608277a93b715d16e9180559de98ebd298898
Igor Todorovski <itodorov@ca.ibm.com>
2023-10-06 15:16:23 -0400

    Revert "Correct shell scripts issues, format shell scripts"
    This reverts commit 39ad0130e520f2f7dfe42bed8a9f1018974660b8.

--- tools/update_stable_releases.sh
+++ tools/update_stable_releases.sh
@@ -2,15 +2,15 @@
 
 # Script to automatically update all or a select number of stable tools
 
-if [ -z "${stabletools}" ]; then
+if [ -z "$stabletools" ]; then
   echo "Updating all stable tools"
   stabletools="sed,grep,gawk,man-db,groff,gdbm,curl,make,git,less,perl,jq,bash,diffutils,findutils,coreutils,tar,gzip,xz,bzip2,vim,ncurses,libiconv"
 fi
 
-for tool in $(echo "${stabletools}" | tr ',' ' '); do
+for tool in $(echo "$stabletools" | tr ',' ' '); do
   ./create_stable_release.sh --repo "${tool}port" --uname ZOSOpenTools
   if [ $? -ne 0 ]; then
-    echo "create_stable_release.sh failed for ${tool}"
+    echo "create_stable_release.sh failed for $tool"
     break
   fi
 done
