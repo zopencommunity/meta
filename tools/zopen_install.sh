06221bbcdc5949d19a7254be6ba62abf4ab8b4f6
Igor Todorovski <itodorov@ca.ibm.com>
2024-06-17 11:58:32 -0400

    Updates to zopen_install.sh script

--- tools/zopen_install.sh
+++ tools/zopen_install.sh
@@ -2,6 +2,7 @@
 # 
 # Quick Install tool for zopen's meta package
 # Assumes you have Curl and Bash installed (likely through Open Enterprise Foundation)
+# Downloads and extracts meta into the current directory
 
 if [[ $(uname) != "OS/390" ]]; then
   echo "Error: This script is only for z/OS systems."
@@ -13,11 +14,13 @@ if ! command -v curl > /dev/null 2>&1; then
   exit 1
 fi
 
+ZOPEN_RELEASE_JSON="https://raw.githubusercontent.com/ZOSOpenTools/meta/main/docs/api/zopen_releases_latest.json"
+
 # Download the latest json
 echo "> Getting latest data from z/OS Open Tools..."
-url=$(curl --fail-with-body --silent -L https://raw.githubusercontent.com/ZOSOpenTools/meta/main/docs/api/zopen_releases_latest.json)
+url=$(curl --fail-with-body --silent -L $ZOPEN_RELEASE_JSON)
 if [ $? -gt 0 ]; then
-  echo "Error: Curl failed due to: \"$url\""
+  echo "Error: Curl failed to download release json $ZOPEN_RELEASE_JSON due to: \"$url\""
   exit 1
 fi
 
@@ -27,14 +30,14 @@ paxFile=$(basename "$url");
 
 echo "> Downloading z/OS Open Tools $url..."
 
-url=$(curl --fail-with-body -O -L "$url")
+response=$(curl --fail-with-body -O -L "$url")
 if [ $? -gt 0 ]; then
-  echo "Error: Curl failed due to: \"$url\""
+  echo "Error: Curl failed to download latest zopen due to: \"$response\"."
   exit 1
 fi
 
 if [ ! -f $paxFile ]; then
-  echo "Error: $paxFile not present"
+  echo "Error: $paxFile not present."
   exit 1
 fi
 
@@ -42,13 +45,13 @@ fi
 echo "> Extracting $paxFile..."
 paxOutput=$(pax -rvf "$paxFile" 2>&1)
 if [ $? -gt 0 ]; then
-   echo "Error: Failed to unpax"
+   echo "Error: Failed to unpax file $paxFile."
   exit 1
 fi
 
 dir=$(echo "$paxOutput" | head -1)
 if [ ! -d "$dir" ]; then
-  echo "Error: $dir is not a valid directory"
+  echo "Error: $dir is not a valid directory."
   exit 1
 fi
 
@@ -61,3 +64,7 @@ source ./.env
 
 echo "> Initializing zopen tools..."
 zopen init
+
+echo "> Cleaning up..."
+rm -rvf $paxFile
+rm -rvf $dir
