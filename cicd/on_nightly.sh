bd42742984c03a17002cd5dc34eb0771bc7ceda7
Sanjana-Kondalwade <sanjana.kondalwade@ibm.com>
2026-07-06 17:48:19 +0530

    updated
    Signed-off-by: Sanjana-Kondalwade <sanjana.kondalwade@ibm.com>

--- cicd/on_nightly.sh
+++ cicd/on_nightly.sh
@@ -4,10 +4,6 @@
 #
 set -e
 
-UpdateGithub() {
-  multi-gitter --config ./cicd/multi-gitter-config run ./bulk-utils/enable_disabled_workflow.sh
-}
-
 UpdateDocs() {
   # Update Progress page in documentation
   git clone git@github.com:zopencommunity/meta.git meta_update
@@ -185,5 +181,4 @@ EOF
   git push origin
 }
 
-UpdateGithub
 UpdateDocs
